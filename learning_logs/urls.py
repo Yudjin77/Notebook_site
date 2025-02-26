4adf1c7f3c3866b4ba49aac8c6706190c0dd0f0d
yulian <ylian.khristoforov@gmail.com>
2025-02-26 22:12:52 +0200

    topic_adde

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     #Home page
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -15,3 +16,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    '''defining a new topic'''
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lerning_logs:topics')
+
+    #Returning empty form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
